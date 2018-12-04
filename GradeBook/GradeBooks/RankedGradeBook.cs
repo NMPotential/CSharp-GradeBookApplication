dcad63ff32e53eff291f4e6de637ee9346dad103
Nathan Mokonz <Nathan.mokonz@gmail.com>
2018-12-04 00:23:02 +0000

    Completed the module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook //Add colon to inheret BaseGradeBook class
+    {
+        public RankedGradeBook(string name) : base(name) //Creates call to base constructor
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
